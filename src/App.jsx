91e9ccedc2a4901e935ccd170a00122aa008b7e7
mgDentist <gmi1987@gmail.com>
2023-10-09 10:43:18 +0400

    prepare project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <header className={styles.appHeader}>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
